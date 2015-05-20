17b0980de06f3488ad9f275b9fd9c13a2a5617cc
ittegrat <ittegrat@users.noreply.github.com>
2015-05-20 22:12:12 +0200

    It looks better now

--- cachematrix.R
+++ cachematrix.R
@@ -3,21 +3,31 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
+makeCacheMatrix <- function(mtx = matrix()) {
+
+  inv <- NULL
+
+  set.matrix <- function(m) {
+    mtx <<- m
+    inv <<- NULL
   }
-  get <- function() x
-  setinverse <- function(inv) m <<- inv
-  getinverse <- function() m
+  get.matrix <- function() {
+    mtx
+  }
+  set.inverse <- function(m) {
+    inv <<- m
+  }
+  get.inverse <- function() {
+    inv
+  }
+  
   list(
-    set = set,
-    get = get,
-    setinverse = setinverse,
-    getinverse = getinverse
+    set = set.matrix,
+    get = get.matrix,
+    set.inverse = set.inverse,
+    get.inverse = get.inverse
   )
+
 }
 
 
@@ -25,14 +35,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  m <- x$getinverse()
-  if(is.null(m)) {
-    data <- x$get()
-    m <- solve(data, ...)
-    x$setinverse(m)
+
+  inv <- x$get.inverse()
+
+  if(is.null(inv)) {
+    mtx <- x$get()
+    inv <- solve(mtx, ...)
+    x$set.inverse(inv)
   } else {
     message("getting cached data")
-    return(m)
+#    return(m)
   }
-  m
+
+  inv
+
 }
